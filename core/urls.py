3a7a77080a29ee815c596948ffe94f7cfe1bceb5
Uladzimir Rashuk <rashuk.vova@gmail.com>
2023-03-22 01:46:31 +0300

    static url added

--- core/urls.py
+++ core/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
 
     path('bot/', include('bot_handler.urls',  namespace=''), name='bot_handler')
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
